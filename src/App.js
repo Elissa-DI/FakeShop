69a384c665a2a4fe2d7b54d4e460cfd9520db17b
Elissa <elissadusabe@gmail.com>
2023-05-30 21:40:02 +0200

    new file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Header from './containers/Header';
-import { BrowserRouter as Router, Switch, Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProductDetail from './containers/ProductDetail';
+import ProductListing from './containers/ProductListing';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-        <Route  />
+        <Route  path="/" exact component={ProductListing}/>
+        <Route path="/product/:productId" exact component={ProductDetail} />
+        <Route>404 Not Found😒</Route>
       </Router>
     </div>
   );
