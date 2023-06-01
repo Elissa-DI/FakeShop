398b7c45a640488885089005e81be5be7edc98b8
Elissa <elissadusabe@gmail.com>
2023-06-01 21:38:23 +0200

    new features

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
   return (
-    <div>ProductListing</div>
+    <div className='ui grid container'>
+       <ProductComponent />
+    </div>
   )
 }
 
