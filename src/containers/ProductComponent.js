e45a15418971682f9920194b0a3e3cf5cb668c83
Elissa <elissadusabe@gmail.com>
2023-06-01 21:45:57 +0200

    new features

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
-  const products = useSelector((state) => state);
+  const products = useSelector((state) => state.allProducts);
+  const { id, title } = product;
   return (
     <div className='four column wide'>
       <div className="iu link cards">
@@ -12,7 +13,7 @@ const ProductComponent = () => {
           </div>
           <div className="content">
             <div className="header">
-              
+              { title }
             </div>
           </div>
         </div>
